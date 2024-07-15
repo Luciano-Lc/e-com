b7ea73c9721468510110eb6bb61f8173e867bb3e
Luciano <cheemeeluciano14@gmail.com>
2024-07-15 11:53:44 +0200

    update homescreen images

---
+++ client/src/slices/userApiSlices.js
@@ -0,0 +1,16 @@
+// import { USER_URL } from "../constants";
+// import { apiSlice } from "./apiSlice"; //  endpoints: (builder) => ({}),
+
+// export const userApiSlice = apiSlice.injectEndpoints({
+//     endpoints: (builder) => ({
+//         login: builder.mutation({
+//             query: () => {
+//                 url: `${USER_URL}/auth`,
+//                     method: 'POST',
+//                         body:data
+//           }
+//       })
+//   }),
+// });
+
+// export const {} = userApiSlice;
