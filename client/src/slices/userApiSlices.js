6e9009c4160ff9ab5cbcfd1055278651cd499950
Luciano <cheemeeluciano14@gmail.com>
2024-07-19 13:24:26 +0200

    set user redux mutation

--- client/src/slices/userApiSlices.js
+++ client/src/slices/userApiSlices.js
@@ -1,16 +1,43 @@
-// import { USER_URL } from "../constants";
-// import { apiSlice } from "./apiSlice"; //  endpoints: (builder) => ({}),
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
 
-// export const userApiSlice = apiSlice.injectEndpoints({
-//     endpoints: (builder) => ({
-//         login: builder.mutation({
-//             query: () => {
-//                 url: `${USER_URL}/auth`,
-//                     method: 'POST',
-//                         body:data
-//           }
-//       })
-//   }),
-// });
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+      keepUnsavedDataFor: 5,
+      providesTags: ["users"],
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+  }),
+});
 
-// export const {} = userApiSlice;
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useLogoutMutation,
+  useProfileMutation,
+} = usersApiSlice;
